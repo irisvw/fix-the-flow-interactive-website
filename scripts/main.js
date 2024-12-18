9ea49640eec54eab52d68776e6c0cd28997af987
Iris <iris.van.willigen@gmail.com>
2024-12-18 11:45:00 +0100

    Fix slideshow

--- scripts/main.js
+++ scripts/main.js
@@ -96,7 +96,7 @@ const artObjectsList = {
       "techniques": "Pottery"
     },
     {
-      "image": "assets/islamic-2.jpg",
+      "image": "assets/islamic2.jpg",
       "displayDate": null,
       "productionCulturalGroup": null,
       "site": null,
@@ -520,100 +520,134 @@ const artObjectsList = {
 let buttonNext = document.querySelector(".button-next");
 let buttonPrevious = document.querySelector(".button-previous");
 
-let slidePrevious = document.querySelector("#slide-0");
-let slideCurrent = document.querySelector("#slide-1");
-let slideNext = document.querySelector("#slide-2");
+// Get the ul element that holds all the images.
+let gallery = document.querySelector('.gallery-slideshow');
+
+// Get the filter buttons.
+let filterAll = document.querySelector('#filter-all');
+let filterGlass = document.querySelector('#filter-glass');
+let filterIslamic = document.querySelector('#filter-islamic');
+let filterPottery = document.querySelector('#filter-pottery');
+let filterTapestry = document.querySelector('#filter-tapestry');
+let allFilters = document.querySelectorAll('.button.filter');
 
 // Generate a shuffled list of techniques.
 let techniques = generateList();
-let techniqueLists = {};
-let currentItem = 0;
 
+// Make an empty object.
+let techniqueLists = {};
+
+// Define slides.
+let slidePrevious = document.querySelector('.previous-item');
+let slideCurrent = document.querySelector('.current-item');
+let slideNext = document.querySelector('.next-item');
+
+// For each object in artObjectsList, add an array for that category to the techniquesList, then add the image to the corresponding category.
 artObjectsList.data.forEach(artObject => {
-  // for each art object, get the technique used
   const technique = artObject.techniques;
-  // if techniqueLists doesn't contain that technique yet;
   if (!techniqueLists[technique]) {
-    // make a new array for that technique and store it in techniqueLists.
     techniqueLists[technique] = [];
   }
-  // then add the artObject to its corresponding list.
   techniqueLists[technique].push(artObject);
 });
 
+// The 'next' button is clicked.
 buttonNext.addEventListener("click", function () {
-  // the current slide becomes the previous slide (why does this work? because i remove it again in line 554 lmao)
+  // The previous slide becomes hidden.
+  slidePrevious.classList.remove("previous-item");
+
+  // The current slide becomes the previous slide.
   slidePrevious = slideCurrent;
-  // if a next slide exists, make it the currentslide
-  if (slideCurrent.nextElementSibling) { slideCurrent = slideCurrent.nextElementSibling; }
+  slidePrevious.classList.remove('current-item');
+  slidePrevious.classList.add('previous-item');
 
-  // slideNext = generateImage();
-  updateNext();
-})
-
-buttonPrevious.addEventListener("click", function () {
-  slideNext = slideCurrent;
-  if (slideCurrent.previousElementSibling) { 
-    slideCurrent = slideCurrent.previousElementSibling; 
+  // The next sibling (or the first element) becomes the current slide.
+  if (slideCurrent.nextElementSibling) {
+    slideCurrent = slideCurrent.nextElementSibling;
   } else {
-
+    slideCurrent = gallery.firstElementChild;
   }
 
-  // slidePrevious = generateImage();
-  updatePrevious();
+  slideCurrent.classList.remove('next-item');
+  slideCurrent.classList.add('current-item');
+
+  if (slideCurrent.nextElementSibling) { 
+    slideCurrent.nextElementSibling.classList.add('next-item');
+  } else {
+    gallery.firstElementChild.classList.add('next-item');
+  }
 })
 
-function updateNext() {
-  // console.log(slideCurrent);
-  currentItem += 1;
-  slidePrevious.classList.add("previous-item");
+// The 'previous' button is clicked.
+buttonPrevious.addEventListener("click", function () {
+  // The next slide becomes hidden.
+  slideNext.classList.remove('next-item');
 
-  if (slideCurrent.classList.contains("previous-item")) { slideCurrent.classList.remove("previous-item"); }
-  if (slideCurrent.classList.contains("next-item")) { slideCurrent.classList.remove("next-item"); }
-}
+  // The current slide becomes the next slide.
+  slideNext = slideCurrent;
+  slideNext.classList.remove('current-item');
+  slideNext.classList.add('next-item');
 
-function updatePrevious() {
-  currentItem -= 1;
-  if (currentItem == -1) { currentItem = list.length };
-  slideNext.classList.add("next-item");
+  // The previous sibling (or the last element) becomes the current slide.
+  if (slideCurrent.previousElementSibling) {
+    slideCurrent = slideCurrent.previousElementSibling;
+  } else {
+    slideCurrent = gallery.lastElementChild;
+  }
 
-  if (slideCurrent.classList.contains("previous-item")) { slideCurrent.classList.remove("previous-item"); }
-  if (slideCurrent.classList.contains("next-item")) { slideCurrent.classList.remove("next-item"); }
+  slideCurrent.classList.remove('previous-item');
+  slideCurrent.classList.add('current-item');
+
+  if (slideCurrent.previousElementSibling) { 
+    slideCurrent.previousElementSibling.classList.add('previous-item');
+  } else {
+    gallery.lastElementChild.classList.add('previous-item');
+  }
+})
+
+filterGlass.addEventListener('click', () => { loadImages('Glass'); setActive(filterGlass); });
+filterIslamic.addEventListener('click', () => { loadImages('Islamic Art'); setActive(filterIslamic); });
+filterPottery.addEventListener('click', () => { loadImages('Pottery'); setActive(filterPottery); });
+filterTapestry.addEventListener('click', () => { loadImages('Tapestry'); setActive(filterTapestry); });
+
+function setActive(type) {
+  allFilters.forEach((filter) => {
+    if (filter.classList.contains('active-filter')) {
+      filter.classList.remove('active-filter');
+    }
+  })
+  type.classList.add('active-filter');
 }
 
 function generateList() {
   let list = [];
-  // For each element in the artObjectsList, add the technique used to list, but only if it is not included in list yet.
   artObjectsList.data.forEach(element => { if (!list.includes(element.techniques)) { list.push(element.techniques) } });
-  // Shuffle the list, so any technique has a chance to appear first.
   return list.sort((a, b) => 0.5 - Math.random());
 }
 
-// function pickImage(category) {
-//   // while picture isnt the category
-//   // pick a random picture from the list
-//   let artObject;
-//   while (artObject.techniques != list[currentItem]) {
-//     artObject = artObjectsList[Math.floor(Math.random() * artObjectsList.length)];
-//   }
-//   return artObject;
-// }
-
 function generateImage(link, source, altText, category) {
+  // Create an image with a caption inside of a figure element that is nested inside an anchor tag, which itself is nested in a list item.
   let listItem = document.createElement('li');
   let anchor = document.createElement('a');
   let figure = document.createElement('figure');
   let caption = document.createElement('figcaption');
   let image = document.createElement('img');
 
+  // Set attributes of the anchor tag.
   anchor.setAttribute('href', link);
   anchor.setAttribute('aria-label', "details");
 
+  // Set attributes of the image.
   image.setAttribute('width', '250');
+  image.setAttribute('class', 'slide-img');
   image.setAttribute('src', source);
   image.setAttribute('alt', altText);
   image.setAttribute('data-category', category);
 
+  // Give the list item the 'slide' class.
+  listItem.setAttribute('class', 'slide');
+
+  // Assembly.
   figure.appendChild(image);
   figure.appendChild(caption);
   anchor.appendChild(figure);
@@ -621,16 +655,36 @@ function generateImage(link, source, altText, category) {
   return listItem;
 }
 
-// console.log(list);
+function loadImages(category) {
+  // Clear the gallery.
+  removeAllChildren(gallery);
 
-// There are four categories: tapestry, islamic art, pottery & glass.
-// There will be one slide for each category: four slides total.
-// When going to the next slide, a random picture from that category will be picked.
+  // Load all images in the corresponding category list.
+  items = techniqueLists[category];
+  items.forEach(element => {
+    let item = generateImage('#', element.image, element.title, element.techniques);
+    gallery.appendChild(item);
+  });
 
-// On page load: shuffle all the categories into an array.
-// Pick a random picture from the first category.
-// On 'next', pick a random picture from the second category.
-// On 'previous', pick a random picture from the last category.
+  // Generate a number between 0 and the amount of pictures in the gallery.
+  random = Math.floor(Math.random() * gallery.childNodes.length)
 
-// Add an event listener to each of the filter buttons.
-// On click, load the first three items of the corresponding array.
+  // If random was 0, make the previous slide the last image in the gallery.
+  slidePrevious = (random === 0) ? gallery.lastChild : gallery.childNodes[random - 1]
+  slidePrevious.classList.add('previous-item');
+
+  // Set the current slide to the one chosen randomly.
+  slideCurrent = gallery.childNodes[random];
+  slideCurrent.classList.add('current-item');
+
+  // if random was equal to the amount of pictures, make the next slide the first image in the gallery.
+  slideNext = (random === (gallery.childNodes.length - 1)) ? gallery.firstChild : gallery.childNodes[random + 1];
+  slideNext.classList.add('next-item');
+}
+
+// Function to remove all child elements
+function removeAllChildren(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+}
